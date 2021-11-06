51e1ab57ca16a11576468f3c840cc7ee4668ccb4
cosnomi <cosnomi@cosnomi.com>
2021-11-06 11:50:50 +0900

    Install some packages manually

--- apt-install.sh
+++ apt-install.sh
@@ -6,3 +6,12 @@ echo "Running apt install"
 apt update -y
 DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 cat $THISPATH/apt-packages.txt | xargs apt install -y
+
+# ripgrep
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ripgrep_13.0.0_amd64.deb
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
