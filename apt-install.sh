889d7bd0d64b42f427f677186616f364252f512c
cosnomi <cosnomi@cosnomi.com>
2022-02-13 08:17:46 +0900

    Avoid ripgrep being downloaded in dotfiles directory

--- apt-install.sh
+++ apt-install.sh
@@ -8,8 +8,8 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y tzdata
 cat $THISPATH/apt-packages.txt | xargs apt install -y
 
 # ripgrep
-curl -LO https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-sudo dpkg -i ripgrep_13.0.0_amd64.deb
+curl -L https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb -o ~/ripgrep_13.0.0_amd64.deb
+sudo dpkg -i ~/ripgrep_13.0.0_amd64.deb
 
 # fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
