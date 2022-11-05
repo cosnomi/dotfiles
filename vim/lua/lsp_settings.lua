9825bc54be743346c056d36eb64b0b8198631d72
cosnomi <cosnomi@cosnomi.com>
2022-11-05 23:05:34 +0900

    Enable efm

--- vim/lua/lsp_settings.lua
+++ vim/lua/lsp_settings.lua
@@ -106,14 +106,11 @@ for _, lsp in ipairs(servers) do
   }
 end
 
--- nvim_lsp.efm.setup{
---   on_attach = on_attach,
---   filetypes = { 'python', 'yaml' },
---   default_config = {
---     cmd = { 'efm-langserver', '-c', '~/.config/efm-langserver/config.yaml' };
---   },
---   capabilities = capabilities
--- }
+nvim_lsp.efm.setup{
+  init_options = {documentFormatting = true},
+  on_attach = on_attach,
+  filetypes = { 'python', 'yaml', 'markdown' },
+}
 
 require("flutter-tools").setup {
   fvm = true,
